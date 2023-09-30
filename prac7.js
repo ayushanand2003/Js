2421d0b3e4142cddfb8719a94953cab53f86790e
AYUSH ANAND <91953396+ayushanand2003@users.noreply.github.com>
2023-09-30 13:44:29 +0530

    30th Sept

--- prac7.js
+++ prac7.js
@@ -7,4 +7,7 @@ const age=20
 
 console.log(`\nName is ${name}\nAge is ${age} \nRepo count is ${repoCount}`);
 
-
+console.log(name.length)
+console.log(name.indexOf('y'));
+console.log(name.toUpperCase())
+console.log(`name is ${name}`)
