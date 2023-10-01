06f38b3601775d266bec99b5171b42f09c0abc8b
AYUSH ANAND <91953396+ayushanand2003@users.noreply.github.com>
2023-10-01 21:57:29 +0530

    1st Oct

---
+++ prac13.js
@@ -0,0 +1,20 @@
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+
+let myCreatedDate = new Date(2023,0,23)
+console.log(myCreatedDate.toString());
+console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date
+const Days =['Sun','Mon','Tue','Thurs','Fri','Sat'];
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay().toString());
+console.log(Days[(newDate.getDay().toString())]);
