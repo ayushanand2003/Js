20ec7db48cb01dd051e1094a597825222f8faa77
AYUSH ANAND <91953396+ayushanand2003@users.noreply.github.com>
2023-10-01 21:31:34 +0530

    1st Oct

---
+++ prac8.js
@@ -0,0 +1,22 @@
+ const score = 400.4;
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const hundreds=10000000;
+console.log(hundreds.toLocaleString('en-US'));
+
+console.log(Math);
+console.log(Math.abs(-56));
+console.log(Math.round(5.7));
+console.log(Math.floor(5.7));
+console.log(Math.ceil(5.7));
+console.log((Math.random()*10)+1);
+
+const min =10;
+const max =20;
+
+console.log(Math.floor(Math.random() * (max-min+1)+min))
