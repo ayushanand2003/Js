d7f9850048dc712833fc72d5eb78efb026da50bf
AYUSH ANAND <91953396+ayushanand2003@users.noreply.github.com>
2023-10-03 12:31:28 +0530

    3rd Oct

--- prac14.js
+++ prac14.js
@@ -6,3 +6,14 @@ console.log(myHeros[2]);
 
 const myArr2 = new Array(1,2,3,4,5,6,7,8)
 console.log(myArr2[4]);
+
+myArr.push(6)
+console.log(myArr);
+myArr.pop()
+console.log(myArr);
+
+myArr.unshift(8)
+console.log(myArr);
+console.log(myArr.indexOf(3));
+
+//slice and splice //imp interview question
