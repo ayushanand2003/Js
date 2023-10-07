3fba0aac9df44862c4e7a68074d9dc53e9771d2f
Ayush Anand <ayushanand141@gmail.com>
2023-10-07 16:23:25 +0530

    7th OCt

--- prac16.js
+++ prac16.js
@@ -1,4 +1,25 @@
 //singleton
 
 //object literals
-const JsUser ={}
+const JsUser ={
+    name:"Ayush",
+    age: 18,
+    email:"ayushanand141@gmail.com",
+
+
+}
+
+JsUser.email="ayushanand@gmail.com"
+// Object.freeze(JsUser)
+JsUser.email="ayushanand@yahoo.com"
+console.log(JsUser);
+
+JsUser.greeting= function(){
+    console.log("hello Js User");
+}
+console.log(JsUser.greeting());
+
+JsUser.greetingtwo= function(){
+    console.log(`hello Js User ${this.name}`);
+}
+console.log(JsUser.greetingtwo());
--- tempCodeRunnerFile.js
+++
@@ -1,1 +0,0 @@
-const accountId= 12453 ;
